6d9c9785c7052c9d7518a341cb355fdf2bf569fc
Eric Yan <xyan@humana.com>
2019-11-06 15:56:03 +0800

    remove unused code

--- src/main/java/com/machinedoll/wonderland/Runner.java
+++ src/main/java/com/machinedoll/wonderland/Runner.java
@@ -6,14 +6,14 @@ import com.machinedoll.wonderland.features.java.ConcurrencyCyclicBarrierExample;
 import com.machinedoll.wonderland.lib.AlgorithmRunner;
 
 public class Runner {
-    public static void prepareRuntimeEnvironment() {
-        AlgorithmFactory.register(BasicArithmetic.class);
-        AlgorithmFactory.register(ConcurrencyCyclicBarrierExample.class);
-    }
+  public static void prepareRuntimeEnvironment() {
+    AlgorithmFactory.register(BasicArithmetic.class);
+    AlgorithmFactory.register(ConcurrencyCyclicBarrierExample.class);
+  }
 
-    public static void main(String[] args) throws Exception {
-        prepareRuntimeEnvironment();
-        AlgorithmRunner runner = AlgorithmFactory.getAlgorithmRunner("BasicArithmetic");
-        runner.run();
-    }
+  public static void main(String[] args) throws Exception {
+    prepareRuntimeEnvironment();
+    AlgorithmRunner runner = AlgorithmFactory.getAlgorithmRunner("ConcurrencyCyclicBarrierExample");
+    runner.run();
+  }
 }
