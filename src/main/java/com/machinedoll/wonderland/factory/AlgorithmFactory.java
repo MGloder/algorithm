682d386723f93cbde64c0896f5bf40c56daad8ba
Eric Yan <yanxuok@gmail.com>
2021-01-04 11:18:09 +0800

    random

--- src/main/java/com/machinedoll/wonderland/factory/AlgorithmFactory.java
+++ src/main/java/com/machinedoll/wonderland/factory/AlgorithmFactory.java
@@ -1,31 +1,30 @@
 package com.machinedoll.wonderland.factory;
 
-import com.machinedoll.wonderland.algorithms.BasicArithmetic;
 import com.machinedoll.wonderland.lib.AlgorithmRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class AlgorithmFactory {
-    private static final AlgorithmFactory instance = new AlgorithmFactory();
-    private static Map<String, Class> algorithmMap = new HashMap<>();
+  private static final AlgorithmFactory instance = new AlgorithmFactory();
+  private static Map<String, Class> algorithmMap = new HashMap<>();
 
-    private AlgorithmFactory() {
-    }
+  private AlgorithmFactory() {
+  }
 
-    public static AlgorithmFactory getInstance() {
-        return instance;
-    }
+  public static AlgorithmFactory getInstance() {
+    return instance;
+  }
 
-    public static void register(Class<? extends AlgorithmRunner> f) {
-        algorithmMap.put(f.getSimpleName(), f);
-    }
+  public static void register(Class<? extends AlgorithmRunner> f) {
+    algorithmMap.put(f.getSimpleName(), f);
+  }
 
-    public static AlgorithmRunner getAlgorithmRunner(String algorithm) throws Exception {
-        Class<? extends AlgorithmRunner> clazz = algorithmMap.get(algorithm);
-        if (clazz == null) {
-            throw new Exception("Error");
-        }
-        return clazz.newInstance();
+  public static AlgorithmRunner getAlgorithmRunner(String algorithm) throws Exception {
+    Class<? extends AlgorithmRunner> clazz = algorithmMap.get(algorithm);
+    if (clazz == null) {
+      throw new Exception("Error");
     }
+    return clazz.newInstance();
+  }
 }
